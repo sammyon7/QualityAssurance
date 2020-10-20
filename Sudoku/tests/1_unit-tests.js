85ad73f81871e1cfccd3d8a7821a047763679d89
Rinaldo Matondang <73150015+MatondangR@users.noreply.github.com>
2020-10-20 10:44:17 +0700

    Update 1_unit-tests.js

--- Sudoku/tests/1_unit-tests.js
+++ Sudoku/tests/1_unit-tests.js
@@ -1,11 +1,3 @@
-/*
- *
- *
- *       FILL IN EACH UNIT TEST BELOW COMPLETELY
- *       -----[Keep the tests in the same order!]----
- *       (if additional are added, keep them at the very end!)
- */
-
 const chai = require('chai');
 const assert = chai.assert;
 
