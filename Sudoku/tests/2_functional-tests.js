21239bcb337fbde3258a4015a78777fe2415d50c
Hamora Hadi <61151507+sammyon7@users.noreply.github.com>
2020-10-20 10:44:49 +0700

    Update 2_functional-tests.js

--- Sudoku/tests/2_functional-tests.js
+++ Sudoku/tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
-
 const chai = require("chai");
 const assert = chai.assert;
 
